d324f20125b844fdcbe0035d49e469aae5eeac6e
zhangjing74 <zhangjing74@baidu.com>
2019-11-16 23:51:05 +0800

    bushu

---
+++ precache-manifest.161dab4606f1c8eaf0fc12cddbd241e0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bc5d28c5fe341e8ae45",
+    "url": "/static/js/vendor.1bc5d28c.js"
+  },
+  {
+    "revision": "2e05745d83b4d7e94e3f",
+    "url": "/static/js/index.2e05745d.js"
+  },
+  {
+    "revision": "2e05745d83b4d7e94e3f",
+    "url": "/static/css/index.3a3d3f62.css"
+  },
+  {
+    "revision": "05c4050b832951802ad9",
+    "url": "/static/js/vue.05c4050b.js"
+  },
+  {
+    "revision": "7253881ecb893ceb68f6",
+    "url": "/static/js/manifest.7253881e.js"
+  },
+  {
+    "revision": "b8a42c4a3b00c4f75bc636f2923eaeca",
+    "url": "/static/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "b451e38c6ee4b03781b1019c518e7894",
+    "url": "/static/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "83409fb3fe88cb94d7d3df5fe97569c9",
+    "url": "/static/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "3742528143340e36915adf388d64060f",
+    "url": "/static/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "4dc6fdc67611efce36a3c500cbe1c0ec",
+    "url": "/static/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "588079df8f1b8fde3b3cd691ee65bf06",
+    "url": "/static/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "047f3021e153c19951f86dd209904243",
+    "url": "/static/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "7264d2ec7553bb96307af4acbe697faf",
+    "url": "/static/img/icons/favicon.ico"
+  },
+  {
+    "revision": "21b39911074e22df0e25858265be76bb",
+    "url": "/static/manifest.json"
+  },
+  {
+    "revision": "0ff69c8fca81469c28f589d5fad0616e",
+    "url": "/static/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "efbe0aa485b3aeaceb8ead9191f0c4c0",
+    "url": "/static/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "7264d2ec7553bb96307af4acbe697faf",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "ec9ee816f65893487bebbb1030eca0c0",
+    "url": "/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,62 @@
+importScripts("/precache-manifest.161dab4606f1c8eaf0fc12cddbd241e0.js");
+
+
+        importScripts('/static/workbox-v3.6.3/workbox-sw.js');
+        workbox.setConfig({
+            modulePathPrefix: '/static/workbox-v3.6.3/'
+        });
+    /**
+ * @file service-worker.js with workbox api
+ * @desc [example](https://workbox-samples.glitch.me/examples/workbox-sw/)
+ * @author zhangjing74(sahala_zj@baiddu.com)
+ */
+
+/* globals workbox */
+workbox.core.setCacheNameDetails({
+    prefix: 'lavas-cache',
+    suffix: 'v1',
+    precache: 'install-time',
+    runtime: 'run-time',
+    googleAnalytics: 'ga'
+});
+// workbox.skipWaiting();
+workbox.clientsClaim();
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+self.addEventListener('message', event => {
+    if (event.data === 'skipWaiting') {
+        self.skipWaiting();
+    }
+});
+
+/**
+ * example runningCache with api
+ */
+ workbox.routing.registerRoute(/^https:\/\/lavas\.baidu\.com\/some\/api/,
+     workbox.strategies.networkFirst());
+
+
+/**
+ * example runningCache with resources from CDN
+ * including maxAge, maxEntries
+ * cacheableResponse is important for CDN
+ */
+// workbox.routing.registerRoute(/^https:\/\/cdn\.baidu\.com/i,
+//     workbox.strategies.cacheFirst({
+//         cacheName: 'lavas-cache-images',
+//         plugins: [
+//             new workbox.expiration.Plugin({
+//                 maxEntries: 100,
+//                 maxAgeSeconds: 7 * 24 * 60 * 60
+//             }),
+//             new workbox.cacheableResponse.Plugin({
+//                 statuses: [0, 200]
+//             })
+//         ]
+//     })
+// );
+
+
+
+

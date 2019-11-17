762a0dac1228b2331150cd21decf3191a901cf51
zhangjing74 <zhangjing74@baidu.com>
2019-11-17 17:42:19 +0800

    切分支

--- core/service-worker.js
+++ core/service-worker.js
@@ -26,8 +26,8 @@ self.addEventListener('message', event => {
 /**
  * example runningCache with api
  */
- workbox.routing.registerRoute(/https:\/\/js\.arcgis\.com\/4\.13|https:\/\/sahalazj-github\.github\.io\/lavas-zj/,
-     workbox.strategies.networkFirst());
+// workbox.routing.registerRoute(/https:\/\/js\.arcgis\.com\/4\.13|https:\/\/sahalazj-github\.github\.io\/lavas-zj/,
+ //    workbox.strategies.networkFirst());
 
 
 /**

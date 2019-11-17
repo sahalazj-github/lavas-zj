569d33e349d15f4bd92dcaef51917b1251b67cf2
zhangjing74 <zhangjing74@baidu.com>
2019-11-17 17:14:41 +0800

    切分支

---
+++ service-worker.js
@@ -0,0 +1,62 @@
+importScripts("/lavas-zj/precache-manifest.5854ba0baab4c2ba00c6c5c37243a35d.js");
+
+
+        importScripts('/lavas-zj/static/workbox-v3.6.3/workbox-sw.js');
+        workbox.setConfig({
+            modulePathPrefix: '/lavas-zj/static/workbox-v3.6.3/'
+        });
+    /**
+ * @file service-worker.js with workbox api
+ * @desc [example](https://workbox-samples.glitch.me/examples/workbox-sw/)
+ * @author zhangjing74(sahala_zj@baiddu.com)
+ */
+
+/* globals workbox */
+workbox.core.setCacheNameDetails({
+    prefix: 'lavas-cache',
+    suffix: 'v1',
+    precache: 'install-time',
+    runtime: 'run-time',
+    googleAnalytics: 'ga'
+});
+// workbox.skipWaiting();
+workbox.clientsClaim();
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+self.addEventListener('message', event => {
+    if (event.data === 'skipWaiting') {
+        self.skipWaiting();
+    }
+});
+
+/**
+ * example runningCache with api
+ */
+ workbox.routing.registerRoute(/https:\/\/js\.arcgis\.com\/4\.13|https:\/\/sahalazj-github\.github\.io\/lavas-zj/,
+     workbox.strategies.networkFirst());
+
+
+/**
+ * example runningCache with resources from CDN
+ * including maxAge, maxEntries
+ * cacheableResponse is important for CDN
+ */
+// workbox.routing.registerRoute(/^https:\/\/cdn\.baidu\.com/i,
+//     workbox.strategies.cacheFirst({
+//         cacheName: 'lavas-cache-images',
+//         plugins: [
+//             new workbox.expiration.Plugin({
+//                 maxEntries: 100,
+//                 maxAgeSeconds: 7 * 24 * 60 * 60
+//             }),
+//             new workbox.cacheableResponse.Plugin({
+//                 statuses: [0, 200]
+//             })
+//         ]
+//     })
+// );
+
+
+
+
